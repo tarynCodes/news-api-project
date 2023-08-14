640353082bd0881eaadebcbd5d5356ce9d4d475f
taryncodes@github.com <taryn.bilsborough@gmail.com>
2023-08-14 15:47:58 +0100

    this commit gets the end point api using fs/promises to return a JSON obj

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,8 +1,8 @@
-const app = require("../app")
-const testData = require("../db/data/test-data")
-const db = require("../db/connection")
-const seed = require("../db/seeds/seed")
-const request = require("supertest")
+const app = require("../app");
+const testData = require("../db/data/test-data");
+const db = require("../db/connection");
+const seed = require("../db/seeds/seed");
+const request = require("supertest");
 
 beforeEach(() => seed(testData));
 afterAll(() => {
@@ -10,19 +10,28 @@ afterAll(() => {
 });
 
 describe("GET api/topics", () => {
-    test("200: responds with a 200 status code and sends an array of topics with properties of slug and description", () => {
-        return request(app)
-        .get('/api/topics')
-        .expect(200)
-        .then((response) => {
-            
-            const {topics} = response.body
-            expect(topics).toEqual(expect.any(Array))
-            expect(topics).toHaveLength(3)
-            topics.forEach((topic) => {
-            expect(topic).toHaveProperty("slug");
-            expect(topic).toHaveProperty("description")
-        })
-        })
-    })
-})
+  test("200: responds with a 200 status code and sends an array of topics with properties of slug and description", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then((response) => {
+        const { topics } = response.body;
+        expect(topics).toEqual(expect.any(Array));
+        expect(topics).toHaveLength(3);
+        topics.forEach((topic) => {
+          expect(topic).toHaveProperty("slug");
+          expect(topic).toHaveProperty("description");
+        });
+      });
+  });
+});
+describe("GET api/", () => {
+  test("200: responds with a 200 status code and responds with a JSON obj with all endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then((response) => {
+        expect(response.body).toEqual(expect.any(Object))
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const {getTopics} = require("../be-nc-news/controllers/topic-controllers")
 const app = express();
+const {readApi} = require("../be-nc-news/controllers/api-controllers")
+
+app.use(express.json())
 
 app.get('/api/topics', getTopics)
+
+app.get('/api', readApi)
 
 
 app.use((err, req, res, next) => {
