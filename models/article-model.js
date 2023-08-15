bff162daa4e5dde1e651d296577283aac48dc4f0
taryncodes@github.com <taryn.bilsborough@gmail.com>
2023-08-15 16:02:19 +0100

    tweaked my select article model

--- models/article-model.js
+++ models/article-model.js
@@ -29,7 +29,7 @@ exports.selectArticle = (article_id) => {
     .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
     .then((result) => {
       const article = result.rows[0];
-      if (!article || Object.keys.length === 0) {
+      if (!article) {
         return Promise.reject({
           status: 404,
           msg: "No article found!",
