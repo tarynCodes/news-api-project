38571368b4618d26626f21818b8456242572ca9d
taryncodes@github.com <taryn.bilsborough@gmail.com>
2023-08-16 16:56:57 +0100

    happy paths for Patch on votes on the article

--- controllers/article-controllers.js
+++ controllers/article-controllers.js
@@ -1,24 +1,30 @@
-
-const {selectArticle, fetchArticles, selectCommentsByArticleId} = require("../models/article-model")
+const {
+  selectVotesByArticleId,
+  selectArticle,
+  fetchArticles,
+  selectCommentsByArticleId,
+} = require("../models/article-model");
 
 exports.getArticles = (request, response, next) => {
-    fetchArticles(request.query.sort_by)
+  fetchArticles(request.query.sort_by)
     .then((articles) => {
-      response.status(200).send({articles})
-    }).catch((err) => {
-        next(err)
+      response.status(200).send({ articles });
     })
-
-}
+    .catch((err) => {
+      next(err);
+    });
+};
 
 exports.getArticleByArticleId = (request, response, next) => {
-    const { article_id } = request.params;
-    selectArticle(article_id).then((article) => {
-        response.status(200).send({article})
-    }).catch((err) => {
-        next(err)
+  const { article_id } = request.params;
+  selectArticle(article_id)
+    .then((article) => {
+      response.status(200).send({ article });
     })
-}
+    .catch((err) => {
+      next(err);
+    });
+};
 
 exports.getCommentsByArticleId = (request, response, next) => {
   const { article_id } = request.params;
@@ -35,6 +41,18 @@ exports.getCommentsByArticleId = (request, response, next) => {
     .catch((err) => {
       next(err);
     });
+};
 
-}
+exports.addVotes = (request, response, next) => {
+  const { article_id } = request.params;
+  const { inc_votes } = request.body;
 
+  selectVotesByArticleId(article_id)
+    .then((article) => {
+      const newTotalVotes = article.votes + inc_votes;
+      return response.status(200).send({...article, votes: newTotalVotes });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
