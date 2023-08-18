3c96d4879ed9d3b066bf679366ac53d7f62ca9dd
taryncodes@github.com <taryn.bilsborough@gmail.com>
2023-08-18 16:27:07 +0100

    added the comment count

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -518,3 +518,25 @@ describe('GET /api/articles', () => {
     })
   })
 })
+
+describe("GET /api/articles/:article_id", () => {
+  test("200: responds with a 200 status code with a comment count by its id", () => {
+    return request(app)
+      .get("/api/articles/1")
+      .expect(200)
+      .then((response) => {
+        const article = response.body.article;
+        expect(article).toMatchObject({
+          article_id: 1,
+          title: "Living in the shadow of a great man",
+          topic: "mitch",
+          author: "butter_bridge",
+          body: "I find this existence challenging",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          comment_count: "11"
+        });
+      });
+  });
+});
--- models/article-model.js
+++ models/article-model.js
@@ -42,7 +42,16 @@ exports.fetchArticles = (topic, sort_by = 'created_at', order = 'desc') => {
 
 exports.selectArticle = (article_id) => {
   return db
-    .query(`SELECT * FROM articles WHERE article_id = $1;`, [article_id])
+    .query(  `
+    SELECT articles.*, 
+    COUNT(comments.comment_id) AS comment_count
+    FROM articles
+    LEFT JOIN comments ON articles.article_id = comments.article_id
+    WHERE articles.article_id = $1
+    GROUP BY articles.article_id;
+    `,
+    [article_id]
+  )
     .then((result) => {
       const article = result.rows[0];
       if (!article) {
